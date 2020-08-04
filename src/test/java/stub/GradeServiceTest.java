a098e63aac81cfb9c8a348f558d28372db8a14ef
maqing <mr.maqing@gmail.com>
2020-08-04 20:35:13 +0800

    [maqing] finished mock and stub with mockito

--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -2,14 +2,29 @@ package stub;
 
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 public class GradeServiceTest {
     /* 需求描述：
     编写GradeService类的单元测试，单元测试calculateAverageGrades方法
     * */
 
+    // @Mock
+    // GradeSystem gradeSystem = new GradeSystem();
+    GradeSystem gradeSystem = Mockito.mock(GradeSystem.class);
+    GradeService gradeService = new GradeService(gradeSystem);
+
     @Test
     public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
         //Assertions.assertEquals(90.0, result);
+        when(gradeSystem.gradesFor(0)).thenReturn(new ArrayList<Double>(Arrays.asList(80.0, 90.0, 100.0)));
+        assertEquals(90.0, gradeService.calculateAverageGrades(0));
     }
 }
